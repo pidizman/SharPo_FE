f7882c330d998d774ff8b43a48ed2c1e132db260
Filip Dvorak <pidizman@gmail.com>
2023-04-17 16:35:24 +0200

    idk

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,6 @@ export default function Home() {
   return <>
     <h1>Hello home page </h1>
 
-    <List name="Summer 2020" date="9. 8. 2020" />
+    <List name="Summer 2021" date="9. 8. 2021" />
   </>
 };
